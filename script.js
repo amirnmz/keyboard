d44e47bac019a7cb8153abd6ac3d2d0b21d1d3b8
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2024-02-10 21:03:39 +0330

    hotfix: add class name dynamicly

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const keyboard = document.querySelectorAll('.keyboard-btn');
 const displayBtn = document.querySelector('.display-btn');
+const container = document.querySelector('body');
+
+function addClassName(key) {
+  keyboard.forEach((element) => {
+    if (element.innerHTML.toLowerCase() === key) {
+      element.classList.add('key');
+    }
+
+    setTimeout(() => {
+      element.classList.remove('key');
+    }, 100);
+  });
+}
 
 function insertChar(char) {
   const charContainer = document.createElement('p');
   charContainer.classList.add('char-container');
   charContainer.innerText = char;
-
   displayBtn.insertAdjacentElement('beforeend', charContainer);
 }
 
 function handleKeyDown(event) {
   if (event.keyCode >= 60 && event.keyCode <= 90) {
+    addClassName(event.key);
     insertChar(event.key);
   } else {
     console.log('something went wrong!');
   }
 }
 
-keyboard.forEach((button) => {
-  button.addEventListener('keydown', handleKeyDown);
+container.addEventListener('keydown', (event) => {
+  handleKeyDown(event);
 });
